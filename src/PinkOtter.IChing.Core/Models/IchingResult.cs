3ea19abc9cd94c7bdd1397150617bffb491b9fce
liha-engineer <liha.engineer@gmail.com>
2026-01-30 01:21:53 +0900

    add: src 디렉토리 추가

---
+++ src/PinkOtter.IChing.Core/Models/IchingResult.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PinkOtter.IChing.Core.Models
+{
+    public class IchingResult
+    {
+        public string Question { get; set; } = "";
+        public int[] Lines { get; set; } = new int[6];
+    }
+}
